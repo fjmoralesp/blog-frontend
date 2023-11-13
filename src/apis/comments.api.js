434094a6a26c9e835342c40f173d4e4e0c6dd58a
Francisco Morales <fjmoralesp@outlook.com>
2023-11-13 15:31:37 -0500

    comments update and delete

--- src/apis/comments.api.js
+++ src/apis/comments.api.js
@@ -17,3 +17,39 @@ export const createComment = async (comment) => {
 
     return await response.json();
 }
+
+export const updateComment = async (comment) => {
+    const token = window.localStorage.getItem('token');
+    const { id, body } = comment;
+    const response = await fetch(`http://localhost:3001/comments?commentId=${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ body }),
+    });
+
+    if (!response.ok) {
+        throw new Error('Cannot update post');
+    }
+
+    return await response.json();
+}
+
+export const deleteComment = async (comment) => {
+    const token = window.localStorage.getItem('token');
+    const { id } = comment;
+    const response = await fetch(`http://localhost:3001/comments?commentId=${id}`, {
+        method: 'DELETE',
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error('Cannot delete post');
+    }
+
+    return await response.json();
+}
--- src/components/comments/comment/Comment.js
+++ src/components/comments/comment/Comment.js
@@ -1,22 +1,107 @@
-import React from 'react';
-import { Typography, Card, CardContent } from '@mui/material';
+import React, {useState} from 'react';
+import { Typography, Card, CardContent, Button, TextField, Box } from '@mui/material';
+import { useUser } from '../../../apis/users.api';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { deleteComment, updateComment } from '../../../apis/comments.api';
 
 function Comment({ comment }) {
+    const queryClient = useQueryClient();
+
+    const user = useUser();
+    const [body, setBody] = useState('');
+    const [editingId, setEditingId] = useState();
+
+    const mutationUpdate = useMutation({
+        mutationFn: updateComment,
+        onSuccess: () => {
+            setEditingId(null);
+            queryClient.invalidateQueries({ queryKey: ['posts'] });
+        },
+    });
+
+    const mutationDelete = useMutation({
+        mutationFn: deleteComment,
+        onSuccess: () => {
+            setEditingId(null);
+            queryClient.invalidateQueries({ queryKey: ['posts'] });
+        },
+    });
+
+    const handleBodyChange = (event) => {
+        setBody(event.target.value);
+    }
+
+    const handleEdit = (comment) => {
+        setEditingId((prev) => (prev === comment.id ? null : comment.id));
+        setBody(comment.body);
+    }
+
+    const handleDelete = (comment) => {
+        mutationDelete.mutate({ id: comment.id });
+    }
+
+    const handleComment = (id) => {
+        mutationUpdate.mutate({ body, id });
+    };
+
+    const isEditing = (id) => editingId === id;
+    const canEdit = (userId) => user.data?.id === userId;
+
     return (
         <Card>
             <CardContent>
+                {canEdit(comment.User.id) && (
+                    <>
+                        <Button
+                            variant="text"
+                            onClick={() => handleEdit(comment)}
+                        >
+                            Edit
+                        </Button>
+                        <Button
+                            variant="outlined"
+                            color="error"
+                            onClick={() => handleDelete(comment)}
+                        >
+                            delete
+                        </Button>
+                    </>
+                )}
                 <Typography sx={{ mb: 1.5 }} color="text.secondary" variant="subtitle2">
                     Comment author
                 </Typography>
                 <Typography variant="body2">
-                    {comment.UserId}
+                    {comment.User.username}
                 </Typography>
                 <Typography sx={{ mb: 1.5 }} color="text.secondary" variant="subtitle2">
                     Comment body
                 </Typography>
-                <Typography variant="body2">
-                    {comment.body}
-                </Typography>
+                {isEditing(comment.id) ? (
+                    <TextField
+                        required
+                        id="body"
+                        label="body"
+                        multiline
+                        rows={4}
+                        value={body}
+                        onChange={handleBodyChange}
+                    />
+                ) : (
+                    <Typography variant="body2">
+                        {comment.body}
+                    </Typography>
+                )}
+                {isEditing(comment.id) && (
+                    <Box sx={{ p: 1 }}>
+                        <Button
+                            variant="contained"
+                            size="large"
+                            onClick={() => handleComment(comment.id)}
+                        >
+                            Save
+                        </Button>
+                    </Box>
+                )}
             </CardContent>
         </Card>
     )
